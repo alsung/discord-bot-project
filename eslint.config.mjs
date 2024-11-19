8f23deeda8b1352994ef7a36aab750466643ad24
Alexander Sung <alexsung98@gmail.com>
2024-11-18 18:29:28 -0800

    update configs, add ts config, build ready

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.ts', '**/*.js'], // Specify file patterns/extensions
+    languageOptions: { 
+      parser: parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: globals.node,
+      ecmaVersion: 2021, 
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      "semi": ["error", "always"], // Example custom rule
+    },
+  },
   pluginJs.configs.recommended,
 ];
